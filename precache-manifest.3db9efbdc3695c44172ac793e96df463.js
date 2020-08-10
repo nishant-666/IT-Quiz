44d7b20d2a6fbbe7a9c216a79332e4e1f329f811
nishant666 <nishants440@gmail.com>
2020-08-10 21:44:53 +0530

    Updates

---
+++ precache-manifest.3db9efbdc3695c44172ac793e96df463.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fb70971078c1146aea1398c31ebdc2",
+    "url": "/IT-Quiz/index.html"
+  },
+  {
+    "revision": "606ad3803dafe511efb4",
+    "url": "/IT-Quiz/static/css/2.ef615af2.chunk.css"
+  },
+  {
+    "revision": "5c16ffb57795406d3ec0",
+    "url": "/IT-Quiz/static/css/main.7d524729.chunk.css"
+  },
+  {
+    "revision": "606ad3803dafe511efb4",
+    "url": "/IT-Quiz/static/js/2.f598e2f3.chunk.js"
+  },
+  {
+    "revision": "5c16ffb57795406d3ec0",
+    "url": "/IT-Quiz/static/js/main.52dce77f.chunk.js"
+  },
+  {
+    "revision": "b8c07b76488fc94a6ea8",
+    "url": "/IT-Quiz/static/js/runtime-main.bd4f2988.js"
+  },
+  {
+    "revision": "a5a8d21a2720d8b357b618ceb16e7356",
+    "url": "/IT-Quiz/static/media/background.a5a8d21a.jpg"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/IT-Quiz/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "ee6aadae18d9da389dc08dd8d72e3721",
+    "url": "/IT-Quiz/static/media/correct-answer.ee6aadae.mp3"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/IT-Quiz/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/IT-Quiz/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/IT-Quiz/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/IT-Quiz/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "c7106542fc4e8075bf1bae4bf6854692",
+    "url": "/IT-Quiz/static/media/wrong-answer.c7106542.mp3"
+  }
+]);
